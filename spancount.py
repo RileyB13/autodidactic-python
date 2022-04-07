b91fec1b5d731e49881f704c08f4d5b32bd67bd0
RileyB13 <rileydbrogan@gmail.com>
2022-04-07 11:40:42 -0400

    Editing spancount.py

--- spancount.py
+++ spancount.py
@@ -1,7 +1,6 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import ssl
-import re
 
 # Ignore SSL certificate errors
 ctx = ssl.create_default_context()
@@ -12,13 +11,7 @@ url = input('Enter - ')
 html = urlopen(url, context=ctx).read()
 soup = BeautifulSoup(html, "html.parser")
 
-# Retrieve all of the span elements
-tags = soup('span')
-numlist = list()
-count = 0
+# Retrieve all of the anchor tags
+tags = soup('a')
 for tag in tags:
-    numlist.append(int(tag.contents[0]))
-    count = count + 1
-
-print(count)
-print(sum(numlist))
+    print(tag.get('href', None))
